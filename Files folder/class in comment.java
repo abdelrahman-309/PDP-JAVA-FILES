79bd2f5d3e041e553eb13a7ecdc23290d660d8ac
abdelrahman-309 <83457701+abdelrahman-309@users.noreply.github.com>
2024-03-11 18:24:02 +0300

    Add files via upload

---
+++ Files folder/class in comment.java
@@ -0,0 +1,19 @@
+package com.company;
+
+
+import java.io.File;
+
+// public class Main
+/*
+public class Main {
+
+    public static void main(String[] args) {
+
+        String localPath = "./FilesDirectory";
+
+        File file = new File(localPath) ;
+
+    }
+}
+
+*/
