813548d39868f844a7660d4dfa96c4a3b7133bca
abdelrahman-309 <83457701+abdelrahman-309@users.noreply.github.com>
2024-03-31 17:44:35 +0300

    Add files via upload

--- Deneme.java
+++ Deneme.java
@@ -24,7 +24,7 @@ public class Main {
 
     public static void main(String[] args) {
 
-        String localPath = "./FilesDirectory";
+        String loc//alPath = "./FilesDirectory";
 
         File file = new File(localPath) ;
 
