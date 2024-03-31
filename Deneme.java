efcb17c2e5e42a7334bc911503714720bc834fd7
abdelrahman-309 <83457701+abdelrahman-309@users.noreply.github.com>
2024-03-31 17:37:06 +0300

    Add files via upload

---
+++ Deneme.java
@@ -0,0 +1,32 @@
+package com.company;
+
+import java.io.File;
+
+// public class Main
+/**/
+/*
+klmkl*/
+
+
+/*
+dasfskalfd
+
+*/
+
+/ *
+* /
+//
+//
+/*
+*dlsakjfkljasdkjf
+*/
+public class Main {
+
+    public static void main(String[] args) {
+
+        String localPath = "./FilesDirectory";
+
+        File file = new File(localPath) ;
+
+    }
+}
